85b7f0c8701e92a1e2f50001976fb19705148df1
EyadHamza <EyadHamza0@outlook.com>
2021-05-15 23:05:12 +0200

    REFACTOR

--- src/Exceptions/NotionDatabaseException.php
+++ src/Exceptions/NotionDatabaseException.php
@@ -4,15 +4,14 @@
 namespace Pi\Notion\Exceptions;
 
 
-use JetBrains\PhpStorm\Pure;
 
 class NotionDatabaseException extends \Exception
 {
-    #[Pure] public static function notFound($id): static
+    public static function notFound($id): static
     {
         return new static("notion database with the id of " . $id . "is not found");
     }
-    #[Pure] public static function notAuthorized(): static
+    public static function notAuthorized(): static
     {
         return new static("API token is invalid. you are not authorized to access the notion database with this token ");
     }
