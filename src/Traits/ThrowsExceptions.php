65f2b869c54bd6b755bf2293b08a31d4054a24c5
EyadHamza <EyadHamza0@outlook.com>
2021-05-16 02:06:17 +0200

    Exceptions

--- src/Traits/ThrowsExceptions.php
+++ src/Traits/ThrowsExceptions.php
@@ -13,14 +13,26 @@ trait ThrowsExceptions
     public function throwExceptions($response)
     {
 
-        if (get_class($this) == 'Pi\Notion\NotionDatabase'){
+
             if ($response->status() == 400){
-                throw NotionDatabaseException::notFound($this->id);
+                throw NotionDatabaseException::notFound($response);
             }
             if ($response->status() == 401){
                 throw NotionDatabaseException::notAuthorized();
             }
-        }
+
 
     }
+
+    //400	"invalid_json"	The request body could not be decoded as JSON.
+//400	"invalid_request_url"	The request URL is not valid.
+//400	"invalid_request	This request is not supported.
+//400	"validation_error"	The request body does not match the schema for the expected parameters. Check the "message" property for more details.
+//401	"unauthorized"	The bearer token is not valid.
+//403	"restricted_resource"	Given the bearer token used, the client doesn't have permission to perform this operation.
+//404	"object_not_found"	Given the bearer token used, the resource does not exist. This error can also indicate that the resource has not been shared with owner of the bearer token.
+//409	"conflict_error"	The transaction could not be completed, potentially due to a data collision. Make sure the parameters are up to date and try again.
+//429	"rate_limited"	This request exceeds the number of requests allowed. Slow down and try again. More details on rate limits
+//500	"internal_server_error"	An unexpected error occurred. Reach out to Notion support.
+//503	"service_unavailable"	Notion is unava
 }
